d68f6c01aa4ede99c36ed27034bd9820a05fad8b
codethebasics <codethebasics@gmail.com>
2022-08-11 15:09:16 -0300

    connect

--- components/layout/footer/Footer.js
+++ components/layout/footer/Footer.js
@@ -1,9 +1,11 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Button } from '@chakra-ui/react'
 
 export default function Footer() {
   return (
     <Box p={5} background='#222' color='#fff'>
-      Footer
+      <Button colorScheme='teal' size='md'>
+        Button
+      </Button>
     </Box>
   )
 }
