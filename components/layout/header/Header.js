7beecdc5bbf2d487daea5ba845de1796cb022fb8
codethebasics <codethebasics@gmail.com>
2022-08-20 15:17:28 -0300

    refactor

--- components/layout/header/Header.js
+++ components/layout/header/Header.js
@@ -1,17 +1,47 @@
-import { Box, Flex, Image } from '@chakra-ui/react'
+import { Box, Flex, Image, Button } from '@chakra-ui/react'
 import styles from './Header.module.scss'
+import { useState, useEffect } from 'react'
+import * as Web3Service from '../../../services/web3.service'
 
 export default function Header() {
+  const [address, setAddress] = useState('')
+
+  /**
+   *
+   */
+  useEffect(() => {
+    const fetchWeb3Info = async () => {
+      if (window.ethereum) {
+        setAddress(await Web3Service.getConnectedAddress())
+        Web3Service.listenNetworkChange()
+      }
+    }
+    fetchWeb3Info()
+  }, [])
+
+  /**
+   *
+   */
+  const connectWallet = async () => {
+    const _address = await Web3Service.connect()
+    setAddress(_address[0])
+  }
+
+  /**
+   *
+   */
+  const shortenWalletAddress = address => {
+    return `${address.substring(0, 4)}...${address.substring(address.length - 4, address.length)}`
+  }
+
   return (
-    <Flex
-      p={5}
-      backgroundColor='#425f97'
-      borderBottom='1px solid #222'
-      className={styles.header}
-      alignItems='center'
-      justifyContent='center'
-    >
-      <Image src='/img/regy.png' alt='regy' width={200} />
-    </Flex>
+    <Box className={styles.header} backgroundColor={'#222'} px={5} py={2}>
+      <Flex alignItems={'center'} justifyContent={'space-between'}>
+        <Image src='/img/regy.png' alt='regy' width={150} />
+        <Button size={'sm'} colorScheme='yellow' onClick={connectWallet}>
+          {address ? shortenWalletAddress(address) : 'Connect'}
+        </Button>
+      </Flex>
+    </Box>
   )
 }
